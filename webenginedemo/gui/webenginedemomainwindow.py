f9df99d2b83225c86d43151f1b2c04c52c9f4966
Jonathan Pickering <J.H.Pickering@leeds.ac.uk>
2020-11-18 10:57:17 +0000

    minor bug fixes

--- webenginedemo/gui/webenginedemomainwindow.py
+++ webenginedemo/gui/webenginedemomainwindow.py
@@ -2,10 +2,15 @@
 """
 Created on Mon 16 Nov 2020
 
-Adapted from Qt Documentation page
-https://doc.qt.io/qt-5/qtwebengine-webenginewidgets-minimal-example.html
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at
+http://www.apache.org/licenses/LICENSE-2.0
 
-Available under GNU Free Documentation License version 1.3
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
 
 This work was funded by Joanna Leng's EPSRC funded RSE Fellowship (EP/R025819/1)
 
@@ -55,9 +60,9 @@ class WebEngineDemoMainWindow(qw.QMainWindow, Ui_WebEngineDemoMainWindow):
         height = 512
         width = 512
 
-        html = "<h1>The Tlite</h1>"
+        html = "<h1>The Title</h1>"
         html += "<h2>HTML Image</h2>"
-        html += f"<img src=\"{path}\" width=\"{width}\" height=\"{height}\">"
+        html += f"<p><img src=\"{path}\" width=\"{width}\" height=\"{height}\">"
         self._doc.setHtml(html)
 
     @qc.pyqtSlot()
